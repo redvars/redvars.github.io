bf035090b5e10dc84b839658b5e98240871b44d5
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2025-11-10 09:25:37 +0000

    rename justaos to redvars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
 import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({site: 'https://justaos.com', compressHTML: false, server: {port: 4000, host: true}});
+export default defineConfig({site: 'https://redvars.com', compressHTML: false, server: {port: 4000, host: true}});
