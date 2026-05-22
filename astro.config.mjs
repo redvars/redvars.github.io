8168cee595769cc6dc534314b34c5b8b0723b22c
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2026-05-22 10:44:29 +0530

    temp keep peacock

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
-export default defineConfig({site: 'https://redvars.com', compressHTML: false, server: {port: 4000, host: true}});
+export default defineConfig({
+  devToolbar: { enabled: false },
+  site: 'https://redvars.com',
+  compressHTML: false,
+  server: { port: 4000, host: true },
+  integrations: [pagefind(), sitemap(), mdx()],
+});
